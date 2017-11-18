ae50fe8125848fcb17658c557149d2b3089196f6
mcdedmorozz <mcdedmorozz@gmail.com>
2017-11-19 01:05:19 +0300

    Create code
    I wrote the code

--- Blink/app/main.c
+++ Blink/app/main.c
@@ -15,11 +15,25 @@
 //! @{
 //*****************************************************************************
 
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_conf.h"
+#include "stm32f10x_rcc.h"
+
+void delay(long cycle);
+void delay(long cycle){
+	while (cycle > 0)
+		cycle--;
+}
 int main(void)
 {
-
+	RCC->APB2ENR |= (1<<4);
+	GPIOC->CRH |= (0x2<<20);
     while(1)
     {
+    	GPIOC->ODR |= (1<<13);
+    	delay(500000);
+    	GPIOC->ODR |= ~(1<<13);
+    	delay(500000);
     }
 }
 //! @}
